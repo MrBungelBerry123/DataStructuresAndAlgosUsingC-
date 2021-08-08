348e3b24f8143fcb039ef6658d9405344c422bb5
Ritesh Saha <riteshsaha214@gmail.com>
2021-08-08 14:22:38 +0530

    [Added namespace]

--- include/sorting/insertionSort.h
+++ include/sorting/insertionSort.h
@@ -1,46 +1,51 @@
 // Author - Ritesh Saha
 
 // Insertion Sort.
-template <typename T>
-static void insertionSort(T *array, int len, int (*compareTo)(T&, T&) ) {
-	for (int firstUnsortedIndex = 1; firstUnsortedIndex < len; firstUnsortedIndex++) {
-		T key = array[firstUnsortedIndex];
-		int i = firstUnsortedIndex - 1;
-		for (; i >= 0 && (*compareTo)(array[i], key) > 0; i--)
-			array[i + 1] = array[i];
-		array[i + 1] = key;
-	}
-}
 
-template <typename T>
-static void insertionSort(T *array, int len) {
-	for (int firstUnsortedIndex = 1; firstUnsortedIndex < len; firstUnsortedIndex++) {
-		T key = array[firstUnsortedIndex];
-		int i = firstUnsortedIndex - 1;
-		for (; i >= 0 && array[i] > key; i--)
-			array[i + 1] = array[i];
-		array[i + 1] = key;
-	}
-}
+namespace algorithms {
 
-template <typename T>
-static void insertionSort(T *array, int len, int gap) {
-	for (int firstUnsortedIndex = gap; firstUnsortedIndex < len; firstUnsortedIndex += gap) {
-		T key = array[firstUnsortedIndex];
-		int i = firstUnsortedIndex - gap;
-		for (; i >= 0 && array[i] > key; i -= gap)
-			array[i + gap] = array[i];
-		array[i + gap] = key;
+	template <typename T>
+	static void insertionSort(T *array, int len, int (*compareTo)(T&, T&) ) {
+		for (int firstUnsortedIndex = 1; firstUnsortedIndex < len; firstUnsortedIndex++) {
+			T key = array[firstUnsortedIndex];
+			int i = firstUnsortedIndex - 1;
+			for (; i >= 0 && (*compareTo)(array[i], key) > 0; i--)
+				array[i + 1] = array[i];
+			array[i + 1] = key;
+		}
 	}
-}
 
-template <typename T>
-static void insertionSort(T *array, int len, int (*compareTo)(T&, T&), int gap) {
-	for (int firstUnsortedIndex = gap; firstUnsortedIndex < len; firstUnsortedIndex += gap) {
-		T key = array[firstUnsortedIndex];
-		int i = firstUnsortedIndex - gap;
-		for (; i >= 0 && (*compareTo)(array[i], key) > 0; i -= gap)
-			array[i + gap] = array[i];
-		array[i + gap] = key;
+	template <typename T>
+	static void insertionSort(T *array, int len) {
+		for (int firstUnsortedIndex = 1; firstUnsortedIndex < len; firstUnsortedIndex++) {
+			T key = array[firstUnsortedIndex];
+			int i = firstUnsortedIndex - 1;
+			for (; i >= 0 && array[i] > key; i--)
+				array[i + 1] = array[i];
+			array[i + 1] = key;
+		}
 	}
+
+	template <typename T>
+	static void insertionSort(T *array, int len, int gap) {
+		for (int firstUnsortedIndex = gap; firstUnsortedIndex < len; firstUnsortedIndex += gap) {
+			T key = array[firstUnsortedIndex];
+			int i = firstUnsortedIndex - gap;
+			for (; i >= 0 && array[i] > key; i -= gap)
+				array[i + gap] = array[i];
+			array[i + gap] = key;
+		}
+	}
+
+	template <typename T>
+	static void insertionSort(T *array, int len, int (*compareTo)(T&, T&), int gap) {
+		for (int firstUnsortedIndex = gap; firstUnsortedIndex < len; firstUnsortedIndex += gap) {
+			T key = array[firstUnsortedIndex];
+			int i = firstUnsortedIndex - gap;
+			for (; i >= 0 && (*compareTo)(array[i], key) > 0; i -= gap)
+				array[i + gap] = array[i];
+			array[i + gap] = key;
+		}
+	}
+	
 }
--- include/sorting/selectionSort.h
+++ include/sorting/selectionSort.h
@@ -1,40 +1,45 @@
 // Author - Ritesh Saha
 
 // Selection Sort
-template <typename T>
-static void selectionSort(T *array, int len, int (*compareTo)(T&, T&) ) {
-	bool swap = false;
-	for (int i = 0, n = len - 1; i < n; i++) {
-		int min = i;
-		for (int firstUnsortedIndex = i + 1; firstUnsortedIndex < len; firstUnsortedIndex++) 
-			if (compareTo(array[firstUnsortedIndex], array[min]) < 0) {
-				min = firstUnsortedIndex;
-				swap = true;
-			}
-		if (swap) {
-			T temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			swap = false;
-		}
-	}
-}
 
-template <typename T>
-static void selectionSort(T *array, int len) {
-	bool swap = false;
-	for (int i = 0, n = len - 1; i < n; i++) {
-		int min = i;
-		for (int firstUnsortedIndex = i + 1; firstUnsortedIndex < len; firstUnsortedIndex++) 
-			if (array[firstUnsortedIndex] < array[min]) {
-				min = firstUnsortedIndex;
-				swap = true;
+namespace algorithms {
+
+	template <typename T>
+	static void selectionSort(T *array, int len, int (*compareTo)(T&, T&) ) {
+		bool swap = false;
+		for (int i = 0, n = len - 1; i < n; i++) {
+			int min = i;
+			for (int firstUnsortedIndex = i + 1; firstUnsortedIndex < len; firstUnsortedIndex++) 
+				if (compareTo(array[firstUnsortedIndex], array[min]) < 0) {
+					min = firstUnsortedIndex;
+					swap = true;
+				}
+			if (swap) {
+				T temp = array[i];
+				array[i] = array[min];
+				array[min] = temp;
+				swap = false;
 			}
-		if (swap) {
-			T temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
-			swap = false;
 		}
 	}
+
+	template <typename T>
+	static void selectionSort(T *array, int len) {
+		bool swap = false;
+		for (int i = 0, n = len - 1; i < n; i++) {
+			int min = i;
+			for (int firstUnsortedIndex = i + 1; firstUnsortedIndex < len; firstUnsortedIndex++) 
+				if (array[firstUnsortedIndex] < array[min]) {
+					min = firstUnsortedIndex;
+					swap = true;
+				}
+			if (swap) {
+				T temp = array[i];
+				array[i] = array[min];
+				array[min] = temp;
+				swap = false;
+			}
+		}
+	}
+
 }
